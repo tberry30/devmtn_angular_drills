7175b8b97f0f58a9cc6f62642002bd8f61239ee5
Tim Berry <timothyaberry@gmail.com>
2015-10-30 14:01:37 -0600

    Finished app2 : arrayApp

--- app2 - An array/service.js
+++ app2 - An array/service.js
@@ -101,6 +101,14 @@ var myData = [
 	{"name": "Odysseus Nash", "email": "purus@rutrum.co.uk", "dateEmployed": "01/08/2016", "phone": "722-7908"}
 ];
 
+angular.module('arrayApp').service('arrayService', function(){
+
+	this.getEmployees = function() {
+		return myData;
+	} 	
+
+});
+
 
 
 
